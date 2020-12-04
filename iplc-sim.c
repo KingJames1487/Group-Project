f891cdc731840dfdf57f02c79b22995a560a5ac4
Ferzor <75498218+Ferzor@users.noreply.github.com>
2020-12-04 17:53:40 -0500

    Update iplc-sim.c

--- iplc-sim.c
+++ iplc-sim.c
@@ -328,12 +328,26 @@ void iplc_sim_process_pipeline_rtype( char *instruction, int dest_reg, int reg1,
 
 void iplc_sim_process_pipeline_lw( int dest_reg, int base_reg, unsigned int data_address)
 {
-  /* you gotta do this one */
+  iplc_sim_push_pipeline_stage();
+
+  pipeline[FETCH].itype = LW;
+  pipeline[FETCH].instruction_address = instruction_address;
+
+  pipeline[FETCH].stage.lw.dest_reg = dest_reg;
+  pipeline[FETCH].stage.lw.base_reg = base_reg;
+  pipeline[FETCH].stage.lw.data_address = data_address;
 }
 
 void iplc_sim_process_pipeline_sw( int src_reg, int base_reg, unsigned int data_address)
 {
-  /* you gotta do this one */
+  iplc_sim_push_pipeline_stage();
+
+  pipeline[FETCH].itype = SW;
+  pipeline[FETCH].instruction_address = instruction_address;
+
+  pipeline[FETCH].stage.sw.src_reg = src_reg;
+  pipeline[FETCH].stage.sw.base_reg = base_reg;
+  pipeline[FETCH].stage.sw.data_address = data_address;
 }
 
 void iplc_sim_process_pipeline_branch( int reg1, int reg2)
